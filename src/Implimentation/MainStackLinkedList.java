71cb3d1f30a002e5a0771fd1a98811c207ea4a54
Ghayoor <ghayoorhussain701@gmail.com>
2023-02-16 16:27:27 +0500

    Implement Stack Using Linked List

---
+++ src/Implimentation/MainStackLinkedList.java
@@ -0,0 +1,12 @@
+package Implimentation;
+
+class MainStackLinkedList {
+    public static void main(String[] args) {
+        StackLinkedList newStack = new StackLinkedList();
+        newStack.push(1);
+        newStack.push(2);
+        newStack.push(3);
+
+        newStack.deleteStack();
+    }
+}
