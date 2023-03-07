b564edea2f12c937a746559ebab2819d030981b1
Ghayoor <ghayoorhussain701@gmail.com>
2023-03-07 17:56:13 +0500

    Design a stack that supports getMin() in O(1) time and O(1) extra space

---
+++ src/Questions/Min_Stack.java
@@ -0,0 +1,34 @@
+package Questions;
+
+import java.util.Stack;
+
+//https://leetcode.com/problems/min-stack/description/
+//Same question Special Stack
+public class Min_Stack {
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack<Integer>();
+
+    public void push(int x) {
+        // only push the old minimum value when the current
+        // minimum value changes after pushing the new value x
+        if (x <= min) {
+            stack.push(min);
+            min = x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        // if pop operation could result in the changing of the current minimum value,
+        // pop twice and change the current minimum value to the last minimum value.
+        if (stack.pop() == min) min = stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
