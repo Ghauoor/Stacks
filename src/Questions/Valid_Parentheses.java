fa9a512589a20fa47f68da1ca8c5af015e43fba8
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-04-10 16:56:35 +0500

    Valid-Parentheses

--- src/Questions/Valid_Parentheses.java
+++ src/Questions/Valid_Parentheses.java
@@ -1,9 +1,18 @@
 package Questions;
 
 import java.util.Stack;
-
+//https://leetcode.com/problems/valid-parentheses/description/
 public class Valid_Parentheses {
-    public boolean isValid(String s) {
+    public static void main(String[] args) {
+        String str = "{()}";
+        if (isValid(str)) {
+            System.out.println("String is Valid");
+        } else {
+            System.out.println("String is InValid");
+        }
+    }
+
+    public static boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < s.length(); i++) {
@@ -27,5 +36,42 @@ public class Valid_Parentheses {
             }
         }
         return stack.isEmpty();
+    }
+
+    /*
+     * #######################################################################
+     * Same Approach but slightly diff style code
+     * */
+    public boolean isValidPara(String str) {
+        Stack<Character> st = new Stack<>();
+
+        for (char ch :
+                str.toCharArray()) {
+            if (st.isEmpty() || ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+                continue;
+            }
+
+            if (ch == ')') {
+                if (st.peek() == '(')
+                    st.pop();
+                else
+                    return false;
+
+            } else if (ch == ']') {
+                if (st.peek() == '[')
+                    st.pop();
+                else
+                    return false;
+            } else if (ch == '}') {
+                if (st.peek() == '{')
+                    st.pop();
+                else
+                    return false;
+            }
+        }
+
+        return st.isEmpty();
+
     }
 }
